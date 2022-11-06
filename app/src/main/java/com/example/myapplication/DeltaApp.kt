1320cadaf2f73abcc44ebf7ac5f3a79cb00420c3
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-11-06 15:49:21 -0300

    System bars color changes

--- app/src/main/java/com/example/myapplication/DeltaApp.kt
+++ app/src/main/java/com/example/myapplication/DeltaApp.kt
@@ -1,14 +1,20 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.navigation.BottomBar
 import com.example.myapplication.ui.navigation.NavGraph
 import com.example.myapplication.viewmodel.RoutinesViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.ui.theme.BackgroundButton
+import com.example.myapplication.ui.theme.Black
+import com.example.myapplication.ui.theme.Green
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun DeltaApp(
@@ -16,6 +22,10 @@ fun DeltaApp(
     viewModel: RoutinesViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(
+        color = Green
+        )
     Scaffold(
         bottomBar = { BottomBar(navController = navController) }
     ) {
