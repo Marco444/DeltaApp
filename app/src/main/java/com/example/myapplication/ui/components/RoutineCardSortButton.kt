198d6ff18332b55dabfc0c852231e2da6fbbfc9c
Marco444 <elscili444.code@gmail.com>
2022-11-10 18:46:02 -0300

    minor changes

--- app/src/main/java/com/example/myapplication/ui/components/RoutineCardSortButton.kt
+++ app/src/main/java/com/example/myapplication/ui/components/RoutineCardSortButton.kt
@@ -8,30 +8,45 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.R
 import com.example.myapplication.data.Routines
+import com.example.myapplication.ui.theme.Green
 import com.example.myapplication.viewmodel.RoutinesViewModel
+
+enum class SortOption {
+    FAVOURITE, DATE, POINTS;
+
+    fun color(selected: SortOption): Color {
+        return if(selected.name == this.name) Black else Gray
+    }
+}
 
 @Composable
 fun RoutineCardSortButton(favoriteHandler: () -> Unit, dateSortHandler: () -> Unit, pointsSortHandler: () -> Unit) {
+    var selected by remember { mutableStateOf(SortOption.FAVOURITE) }
+
     Row (
         modifier = Modifier
             .clip(RoundedCornerShape(20.dp))
-            .background(Color.Gray)
+            .background(Gray)
             .padding(5.dp),
         ){
-        SortButton(onClick = favoriteHandler, txt = stringResource(R.string.favourite))
+        SortButton(onClick = {favoriteHandler(); selected = SortOption.FAVOURITE}, txt = stringResource(R.string.favourite), color = SortOption.FAVOURITE.color(selected))
         Delimiter()
-        SortButton(onClick = dateSortHandler, txt = stringResource(R.string.date))
+        SortButton(onClick = {dateSortHandler(); selected = SortOption.DATE}, txt = stringResource(R.string.date), color = SortOption.DATE.color(selected))
         Delimiter()
-        SortButton(onClick = pointsSortHandler, txt = stringResource(R.string.points))
+        SortButton(onClick = {pointsSortHandler(); selected = SortOption.POINTS}, txt = stringResource(R.string.points), color = SortOption.POINTS.color(selected))
     }
 }
 
@@ -46,13 +61,13 @@ fun Delimiter() {
 }
 
 @Composable
-fun SortButton(onClick: () -> Unit, txt: String) {
-    Box(Modifier.clickable(onClick = onClick)) {
+fun SortButton(onClick: () -> Unit, txt: String, color: Color) {
+    Box(Modifier.clickable(onClick = onClick).padding(10.dp)) {
         Text(
             text = txt,
             fontSize = 25.sp,
-            color = Color.White,
-            modifier = Modifier.padding(6.dp),
+            color = White,
+            modifier = Modifier.clip(RoundedCornerShape(5.dp)).background(color),
         )
     }
 }
