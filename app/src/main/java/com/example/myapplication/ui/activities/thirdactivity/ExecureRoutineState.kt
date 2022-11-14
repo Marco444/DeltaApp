6531a5ffbec4012f12103fbb6516041aad18f576
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-11-14 11:24:51 -0300

    Alternative routine exec

--- app/src/main/java/com/example/myapplication/ui/activities/thirdactivity/ExecureRoutineState.kt
+++ app/src/main/java/com/example/myapplication/ui/activities/thirdactivity/ExecureRoutineState.kt
@@ -7,9 +7,20 @@ import com.example.myapplication.data.Routines
 class ExecuteRoutine{
 
     var exercises: RoutineExercises = RoutineExercises(
-        mutableListOf(Exercise(0,0,15f,15f,1,"Pecho1","aaa")),
-        mutableListOf(Exercise(0,1,15f,15f,1,"Descanso","aaa",false)),
-        mutableListOf(Exercise(0,2,15f,15f,1,"Pecho3","aaa")))
+        mutableListOf(Exercise(0,0,15f,15f,1,"Cinta","Trote moderado para entrar en calor"),
+            Exercise(0,0,15f,15f,1,"Bench Press","Sentate y hace fierros qcyo hermano"),
+            Exercise(0,0,15f,15f,1,"Banco inclinado","Algo mas por aca"),
+            Exercise(0,0,15f,15f,1,"Vuelos lateras","Guarda con los hombros")
+        ),
+        mutableListOf(Exercise(0,0,15f,15f,1,"Cinta","Trote moderado para entrar en calor"),
+            Exercise(0,0,15f,15f,1,"Bench Press","Sentate y hace fierros qcyo hermano"),
+            Exercise(0,0,15f,15f,1,"Banco inclinado","Algo mas por aca"),
+            Exercise(0,0,15f,15f,1,"Vuelos lateras","Guarda con los hombros")
+        ),
+        mutableListOf(Exercise(0,0,15f,15f,1,"Cinta","Trote moderado para entrar en calor"),
+            Exercise(0,0,15f,15f,1,"Bench Press","Sentate y hace fierros qcyo hermano"),
+            Exercise(0,0,15f,15f,1,"Banco inclinado","Algo mas por aca"),
+            Exercise(0,0,15f,15f,1,"Vuelos lateras","Guarda con los hombros")))
     var currentRoutine : Routines = Routines(0,0,"","Pecho", added = true)
     var routineId : Int = 0
 }
