2acbda6f09777de8f8850dc4046c830e304d4423
Marco444 <elscili444.code@gmail.com>
2022-11-16 21:04:19 -0300

    probably better way to close it moving the code down to more base layers

--- app/src/main/java/com/example/myapplication/ui/activities/secondactivity/RoutinesState.kt
+++ app/src/main/java/com/example/myapplication/ui/activities/secondactivity/RoutinesState.kt
@@ -16,38 +16,6 @@ class RoutinesState {
     var userRoutines by mutableStateOf(emptyList<MutableStateFlow<Routines>>())
 
     var exploreRoutines by mutableStateOf((emptyList<MutableStateFlow<Routines>>()))
-//            MutableStateFlow(
-//                Routines(0, R.drawable.registration_background,
-//                "This is a sample routine text", "PUSH", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(1, R.drawable.registration_background,
-//                "This is a sample routine text", "PULL", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(2, R.drawable.registration_background,
-//                "This is a sample routine text", "PUSH", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(3, R.drawable.registration_background,
-//                "This is a sample routine text", "PULL 3", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(4, R.drawable.registration_background,
-//                "This is a sample routine text", "PUSH 4", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(5, R.drawable.registration_background,
-//                "This is a sample routine text", "PULL 5", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(6, R.drawable.registration_background,
-//                "This is a sample routine text", "PUSH 6", false)
-//            ),
-//            MutableStateFlow(
-//                Routines(7, R.drawable.registration_background,
-//                "This is a sample routine text", "PULL 7", false, points = 10)
-//            )
 
 }
 data class PagedRoutines(
@@ -55,8 +23,3 @@ data class PagedRoutines(
     val page : Int,
     val isLastPage : Boolean
 )
-//private fun encoder(filePath: String): String{
-//    val bytes = File(filePath).readBytes()
-//    val base64 = Base64.getEncoder().encodeToString(bytes)
-//    return base64
-//}
