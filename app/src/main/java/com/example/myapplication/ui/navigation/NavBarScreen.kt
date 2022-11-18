1b9a28c7b6509824c3495cd3903bab350542d767
Marco444 <elscili444.code@gmail.com>
2022-11-18 15:31:05 -0300

    added string.xml '

--- app/src/main/java/com/example/myapplication/ui/navigation/NavBarScreen.kt
+++ app/src/main/java/com/example/myapplication/ui/navigation/NavBarScreen.kt
@@ -23,6 +23,6 @@ sealed class NavBarScreen(val title: String, val icon: ImageVector, private val 
     object Routines: NavBarScreen("Routines", Icons.Default.FitnessCenter , route_ = "routines_screen")
     object Progress: NavBarScreen("Progress", Icons.Default.BarChart, route_ = "progress_screen")
     object Explore: NavBarScreen("Explore", Icons.Default.Search, route_ = "explore_screen")
-    object Profile : NavBarScreen("Profile", Icons.Default.Person, route_ = "profile")
+    object Profile : NavBarScreen("Profile", Icons.Default.AccountCircle, route_ = "profile")
 }
 
