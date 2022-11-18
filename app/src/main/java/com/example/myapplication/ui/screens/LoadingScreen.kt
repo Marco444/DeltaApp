01d2472c22e25c762ce441d65656a94d77ccada5
martinippo01 <84152110+martinippo01@users.noreply.github.com>
2022-11-17 23:42:52 -0300

    Loading Screen

---
+++ app/src/main/java/com/example/myapplication/ui/screens/LoadingScreen.kt
@@ -0,0 +1,105 @@
+package com.example.myapplication.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.Green
+import com.example.myapplication.ui.theme.backGround
+import kotlinx.coroutines.delay
+
+@Preview
+@Composable
+fun Testing(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = backGround),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LoadingScreen()
+    }
+}
+
+
+@Composable
+fun LoadingScreen(
+    circleColor: Color = Color(Green.value),
+    circleSize: Dp = 20.dp,
+    animationDelay: Int = 600,
+    initialAlpha: Float = 0.3f
+) {
+
+    // 3 circles
+    val circles = listOf(
+        remember {
+            Animatable(initialValue = initialAlpha)
+        },
+        remember {
+            Animatable(initialValue = initialAlpha)
+        },
+        remember {
+            Animatable(initialValue = initialAlpha)
+        }
+    )
+
+    circles.forEachIndexed { index, animatable ->
+
+        LaunchedEffect(Unit) {
+
+            // Use coroutine delay to sync animations
+            delay(timeMillis = (animationDelay / circles.size).toLong() * index)
+
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = animationDelay
+                    ),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+        }
+    }
+
+    // container for circles
+    Row(
+        modifier = Modifier
+        //.border(width = 2.dp, color = Color.Magenta)
+    ) {
+
+        // adding each circle
+        circles.forEachIndexed { index, animatable ->
+
+            // gap between the circles
+            if (index != 0) {
+                Spacer(modifier = Modifier.width(width = 6.dp))
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(size = circleSize)
+                    .clip(shape = CircleShape)
+                    .background(
+                        color = circleColor
+                            .copy(alpha = animatable.value)
+                    )
+            ) {
+            }
+        }
+    }
+}
